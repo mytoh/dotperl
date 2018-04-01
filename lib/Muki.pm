2f1b4aa7d2350cfb8c9bafd9d58a67491410b665
mytoh <mytoh@gmail.com>
2018-04-01 18:20:15 +0900

    move some functions to util.pm

--- lib/Muki.pm
+++ lib/Muki.pm
@@ -17,25 +17,6 @@ sub new {
     return bless {}, $class;
 }
  
-# [[http://pppurple.hatenablog.com/entry/2016/05/28/230722][高階関数perl（higher order perl） - abcdefg.....]]
-sub map :prototype($&$) ($self, $sub, $list)  {
-  if (is_plain_arrayref($list)) {
-    my @arr = $list->@*;
-    my @res = map { $sub->($_) } @arr;
-    \@res;
-  } else {
-    undef;
-  }  
-}
-
-# [[https://stackoverflow.com/questions/5166662/perl-what-is-the-easiest-way-to-flatten-a-multidimensional-array][list - Perl: What is the easiest way to flatten a multidimensional array? - Stack Overflow]]
-sub flatten :prototype($$) ($self, $list)  {
-  $self->map(sub ($x) { $x->@*}, $list);
-}
-
-sub flatten_all :prototype($$) ($self, $list)  {
-  map { is_plain_arrayref($_) ? $self->flatten_all($_) : $_ } $list->@*;
-}  
 
 
 # __PACKAGE__->meta->make_immutable();
--- lib/Muki/Util.pm
+++ lib/Muki/Util.pm
@@ -11,6 +11,7 @@ use open qw<:std :encoding(UTF-8)>;
 use experimental qw<signatures re_strict refaliasing script_run>;
 use re 'strict';
 use List::AllUtils qw<reduce>;
+use Ref::Util qw<is_plain_arrayref>;
 
 use Exporter 'import';
 our @EXPORT_OK = qw<pipeline compose>;
@@ -36,5 +37,25 @@ sub compose (@fns) {
   };
   $sub;
 }
+
+# [[http://pppurple.hatenablog.com/entry/2016/05/28/230722][高階関数perl（higher order perl） - abcdefg.....]]
+sub map :prototype($&$) ($self, $sub, $list)  {
+  if (is_plain_arrayref($list)) {
+    my @arr = $list->@*;
+    my @res = map { $sub->($_) } @arr;
+    \@res;
+  } else {
+    undef;
+  }  
+}
+
+# [[https://stackoverflow.com/questions/5166662/perl-what-is-the-easiest-way-to-flatten-a-multidimensional-array][list - Perl: What is the easiest way to flatten a multidimensional array? - Stack Overflow]]
+sub flatten :prototype($$) ($self, $list)  {
+  $self->map(sub ($x) { $x->@*}, $list);
+}
+
+sub flatten_all :prototype($$) ($self, $list)  {
+  map { is_plain_arrayref($_) ? $self->flatten_all($_) : $_ } $list->@*;
+}  
 
 !!1;
