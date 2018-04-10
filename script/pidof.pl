ae22824bfff3aae532e7d9228626a37a4562c98b
mytoh <mytoh@gmail.com>
2018-04-11 01:15:35 +0900

    * script/pidof.pl: add

---
+++ script/pidof.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use feature ":5.28";
+use feature qw<refaliasing  declared_refs>;
+use utf8;
+use strictures 2;
+use experimental qw<signatures re_strict refaliasing script_run alpha_assertions>;
+use autodie ':all';
+use utf8::all;
+use open qw<:std :encoding(UTF-8)>;
+use re 'strict';
+use Unix::PID;
+no indirect 'fatal';
+no bareword::filehandles;
+no autovivification;
+
+my sub main ($args) {
+  my $command = $args->[0];
+  my $pid = Unix::PID->new;
+  my @pids = $pid->get_pidof($command);
+  say join(' ', @pids);
+}
+
+main(\@ARGV);
