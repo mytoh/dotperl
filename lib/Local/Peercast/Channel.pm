f329782a122c5e06cf5a31362e5598b79b60b43c
mytoh <mytoh@gmail.com>
2018-08-14 00:02:42 +0900

    * lib/Local/Peercast/Channel.pm: use Mu

--- lib/Local/Peercast/Channel.pm
+++ lib/Local/Peercast/Channel.pm
@@ -1,25 +1,24 @@
 package Local::Peercast::Channel;
 
-use Moo;
+use Mu;
 use strictures 2;
 use namespace::clean;
 
-has [qw< name
-         channel_id
-         tracker
-         contact_url
-         genre
-         description
-         listeners
-         relays
-         bitrate
-         content_type
-         artist
-         album
-         track_title
-         track_url
-         uptime
-         comment >]
-  => (is => 'ro',);
+ro [qw< name
+        channel_id
+        tracker
+        contact_url
+        genre
+        description
+        listeners
+        relays
+        bitrate
+        content_type
+        artist
+        album
+        track_title
+        track_url
+        uptime
+        comment>];
 
 !!1;
