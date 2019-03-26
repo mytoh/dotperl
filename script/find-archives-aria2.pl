75b102c30b3a7bfc9c44170441858d9f83d7e511
mytoh <mytoh@gmail.com>
2019-03-27 01:17:16 +0900

    * script/find-archives-aria2.pl: use Path::Iterator::Rule

--- script/find-archives-aria2.pl
+++ script/find-archives-aria2.pl
@@ -5,33 +5,33 @@ use strict;
 use warnings;
 use utf8;
 use utf8::all;
-use File::Find::Rule;
+use Path::Iterator::Rule;
 use experimental qw<signatures>;
 
 my sub find_files ($dir) {
-    my @files = File::Find::Rule->file()->name( '*.zip', '*.rar' )->in($dir);
+  Path::Iterator::Rule->new->name( '*.zip', '*.rar' )->all($dir);
 }
 
 my sub delete_files ($files) {
-    for my $f ( $files->@* ) {
-        my $fa = $f . ".aria2";
-        say "Deleting file $f";
-        unlink $f or warn "Could not delete $f";
-        say "Deleted file $f";
-        say "Deleting aria2 file $fa";
-        unlink $fa or warn "Could not delete $fa";
-        say "Deleted aria2 file $fa";
-    }
+  for my $f ( $files->@* ) {
+    my $fa = $f . ".aria2";
+    say "Deleting file $f";
+    unlink $f or warn "Could not delete $f";
+    say "Deleted file $f";
+    say "Deleting aria2 file $fa";
+    unlink $fa or warn "Could not delete $fa";
+    say "Deleted aria2 file $fa";
+  }
 }
 
 my sub main ($dir) {
-    my @files = find_files($dir);
+  my @files = find_files($dir);
 
-    my @files_with_aria2 = grep { -e $_ . ".aria2" } @files;
+  my @files_with_aria2 = grep { -e $_ . ".aria2" } @files;
 
-    say $_ for @files_with_aria2;
+  say $_ for @files_with_aria2;
 
-    # delete_files(\@files_with_aria2);
+  # delete_files(\@files_with_aria2);
 
 }
 
